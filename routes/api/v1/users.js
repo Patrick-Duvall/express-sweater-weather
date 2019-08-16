7b8daaa49cf3d8657ab05742d7caaa732876f82f
Patrick Duvall <duvallp208@gmail.com>
2019-08-16 11:28:57 -0600

    add Sad path

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -9,6 +9,8 @@ const saltRounds = 10;
 router.post("/", function (req, res) {
   let api_key = uuidv4()
   let hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  if (req.body.password === req.body.password_confirmation) {
+
 
   User.create({
     email: req.body.email,
@@ -23,6 +25,10 @@ router.post("/", function (req, res) {
       res.setHeader("Content-Type", "application/json");
       res.status(500).send({ error });
     });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(409).send({ error })
+  }
 });
 
 module.exports = router
